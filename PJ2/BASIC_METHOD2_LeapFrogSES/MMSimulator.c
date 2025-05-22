b9bcf239a01b38d82433c6c2d3d030bc16795b55
Yanxiang-ZHU <zhu.yx.0073@gmail.com>
2025-05-22 21:04:24 +0800

    commit v5.22.2

--- PJ2/BASIC_METHOD2_LeapFrogSES/MMSimulator.c
+++ PJ2/BASIC_METHOD2_LeapFrogSES/MMSimulator.c
@@ -7,6 +7,75 @@
 #include "config.h"
 #include "simulator.h"
 
+
+AEvent a;
+AEvent a_prev;
+DEvent d0;
+DEvent d;
+DEvent d_prev;
+
+int tD = 0;
+int initial = 1;
+int wait_time = 0;
+int Vdlast = 0;
+int finish = 0;
+int i = 1;
+
+AEvent initialize_first_event(void) {
+    // initialize event (T = 0) a0
+    AEvent a = {0, (rand() % 1001) / 1000.0, dT_A, Vth, 0};         // parameters: T, V, dT, Vth, bM
+    return a;
+}
+
+void waiting_for_first_D (FILE* fout){
+    if (a.bM != 1) {
+        fprintf(fout, "%.3lf  A(%.3lf %.3lf %.3lf %-2d)\n", a.T, a.V, a.Vth, a.dT, a.bM);
+    } else {
+        initial = 0;     // begin to generate the first D event
+        DEvent DGEN = {a.T, 0, (101 + rand() % 199) / 1000.0, -1};
+        d0 = DGEN;
+        Vdlast = 0;
+        fprintf(fout, "%.3lf  A(%.3lf %.3lf %.3lf %-2d)  D(%d %.3lf %-2d)\n", a.T, a.V, a.Vth, a.dT, a.bM, d0.V, d0.dT, d0.bM);
+        d_prev = d0;
+        d = generate_next_D(d0, a.T);
+    }
+    a_prev = a;
+    a_event_queue[0] = a;
+    a = generate_next_A(a, a.T, Vth);
+}
+
+int continue_all_A (FILE* fout) {
+    if (a_event_queue[i].bM == 1) {
+        DEvent DGEN = {a_event_queue[i].T, Vdlast * (int)(a_event_queue[i].V >= a_event_queue[i].Vth), (101 + rand() % 199) / 1000.0, -1};
+        d = DGEN;
+        fprintf(fout, "%.3lf  A(%.3lf %.3lf %.3lf %-2d)  D(%d %.3lf %-2d)\n", a_event_queue[i].T, a_event_queue[i].V, a_event_queue[i].Vth, a_event_queue[i].dT, a_event_queue[i].bM, d.V, d.dT, d.bM);
+        Vdlast = d.V;
+        d_prev = d;
+        d = generate_next_D(d, a_event_queue[i].T);
+        a_event_queue[0] = a_event_queue[i];
+        i = 1;
+        return 1;
+    } else {
+        fprintf(fout, "%.3lf  A(%.3lf %.3lf %.3lf %-2d)\n", a_event_queue[i].T, a_event_queue[i].V, a_event_queue[i].Vth, a_event_queue[i].dT, a_event_queue[i].bM);
+    }
+    return 0;
+}
+
+void D_happen (FILE* fout) {
+    AEvent AGEN = {d.T, d.V, dT_A, Vth, -1}; 
+    a = AGEN;
+    fprintf(fout, "%.3lf  A(%.3lf %.3lf %.3lf %-2d)  D(%d %.3lf %-2d)\n", d.T, a.V, a.Vth, a.dT, a.bM, d.V, d.dT, d.bM);
+    Vdlast = d.V;
+    a_prev = a;
+    a_event_queue[0] = a;
+    d_prev = d;
+    a = generate_next_A(a, d.T, Vth);  // d.T = t[n+1]
+    d = generate_next_D(d, d.T);
+    i = 1;
+}
+
+
+
 int main() {
     srand(time(NULL));
 
@@ -17,55 +86,27 @@ int main() {
 
     // read config from text
     read_config("con.txt");
-
-    // initialize event (T = 0) a0
-    AEvent a = {0, random_double(0, 1), dT_A, Vth, 0};         // parameters: T, V, dT, Vth, bM
-    AEvent a_prev;
-    DEvent d0;
-    DEvent d;
-    DEvent d_prev;
-
-    int tD = 0;
-    int initial = 1;
-    int wait_time = 0;
-    int Vdlast = 0;
-    int finish = 0;
-    int i = 1;
-
-    // output to file <sim_res.txt>, filename is adjustable
     FILE* fout = fopen("sim_res.txt", "w");
+
+    a = initialize_first_event(); 
 
     while (1) {
         if (a.T > Tsim || finish) break;
 
         if (initial == 1) {
-            if (a.bM != 1) {
-                fprintf(fout, "%.3lf  A(%.3lf %.3lf %.3lf %-2d)\n", a.T, a.V, a.Vth, a.dT, a.bM);
-            } else {
-                initial = 0;     // begin to generate the first D event
-                DEvent DGEN = {a.T, 0, random_double(0.1, 0.3), -1};
-                d0 = DGEN;
-                Vdlast = 0;
-                fprintf(fout, "%.3lf  A(%.3lf %.3lf %.3lf %-2d)  D(%d %.3lf %-2d)\n", a.T, a.V, a.Vth, a.dT, a.bM, d0.V, d0.dT, d0.bM);
-                d_prev = d0;
-                d = generate_next_D(d0, a.T);
-            }
-            a_prev = a;
-            a_event_queue[0] = a;
-            a = generate_next_A(a, a.T, Vth);
+            waiting_for_first_D(fout);
         }
 
         else {
             tD = rand() % 10 + 1; // 1~10
             usleep(tD * 1000);    // sleep for tD ms (tD * 1000 us)
             wait_time += tD;
-            // D reach t[n+1]
+            fprintf(fout, "***waiting for tD: %d ms***\n", tD);
 
             // find A's progress
             // case1: 
             if (dT_A / tA < d_prev.dT / tD) {  // A not reach t[n+1]
                 // find if any bM=1 in A's current progress
-                
                 for (i = 1; i <= (tD/tA); i++) {
                     a_event_queue[i] = generate_next_A(a_event_queue[i-1], a_event_queue[i-1].T, Vth);
                     if (a_event_queue[i].T > Tsim) {
@@ -74,18 +115,8 @@ int main() {
                         break;
                     }
 
-                    if (a_event_queue[i].bM == 1) {
-                        DEvent DGEN = {a_event_queue[i].T, Vdlast * (int)(a_event_queue[i].V >= a_event_queue[i].Vth), random_double(0.1, 0.3), -1};
-                        d = DGEN;
-                        fprintf(fout, "%.3lf  A(%.3lf %.3lf %.3lf %-2d)  D(%d %.3lf %-2d)\n", a_event_queue[i].T, a_event_queue[i].V, a_event_queue[i].Vth, a_event_queue[i].dT, a_event_queue[i].bM, d.V, d.dT, d.bM);
-                        Vdlast = d.V;
-                        d_prev = d;
-                        d = generate_next_D(d, a_event_queue[i].T);
-                        a_event_queue[0] = a_event_queue[i];
-                        i = 1;
+                    if (continue_all_A(fout)) {
                         break;
-                    } else {
-                        fprintf(fout, "%.3lf  A(%.3lf %.3lf %.3lf %-2d)\n", a_event_queue[i].T, a_event_queue[i].V, a_event_queue[i].Vth, a_event_queue[i].dT, a_event_queue[i].bM);
                     }
                 }
                 // no bM=1 in A's current progress
@@ -94,6 +125,8 @@ int main() {
                         // A process
                         usleep(tA * 1000);    // sleep for tA ms (tA * 1000 us)
                         wait_time += tA;
+                        fprintf(fout, "***waiting for tA: %d ms***\n", tA);
+
                         a_event_queue[i] = generate_next_A(a_event_queue[i-1], a_event_queue[i-1].T, Vth);
                         if (a_event_queue[i].T > Tsim) {
                             finish = 1;
@@ -101,33 +134,19 @@ int main() {
                             break;
                         }
     
-                        if (a_event_queue[i].bM == 1) {
-                            DEvent DGEN = {a_event_queue[i].T, Vdlast * (int)(a_event_queue[i].V >= a_event_queue[i].Vth), random_double(0.1, 0.3), -1};
-                            d = DGEN;
-                            fprintf(fout, "%.3lf  A(%.3lf %.3lf %.3lf %-2d)  D(%d %.3lf %-2d)\n", a_event_queue[i].T, a_event_queue[i].V, a_event_queue[i].Vth, a_event_queue[i].dT, a_event_queue[i].bM, d.V, d.dT, d.bM);
-                            Vdlast = d.V;
-                            d_prev = d;
-                            d = generate_next_D(d, a_event_queue[i].T);
-                            a_event_queue[0] = a_event_queue[i];
-                            i = 1;
+                        if (continue_all_A(fout)) {
                             break;
-                        } else {
-                            fprintf(fout, "%.3lf  A(%.3lf %.3lf %.3lf %-2d)\n", a_event_queue[i].T, a_event_queue[i].V, a_event_queue[i].Vth, a_event_queue[i].dT, a_event_queue[i].bM);
                         }
                     }
                 }
                 // no bM=1 in A's whole progress from t[n] to t[n+1] ---- A simulation bM=-1 at t[n+1]
                 if ((i * dT_A) > d_prev.dT) {
-                    AEvent AGEN = {d.T, d.V, dT_A, Vth, -1}; 
-                    a = AGEN;
-                    fprintf(fout, "%.3lf  A(%.3lf %.3lf %.3lf %-2d)  D(%d %.3lf %-2d)\n", d.T, a.V, a.Vth, a.dT, a.bM, d.V, d.dT, d.bM);
-                    Vdlast = d.V;
-                    a_prev = a;
-                    a_event_queue[0] = a;
-                    d_prev = d;
-                    a = generate_next_A(a, d.T, Vth);  // d.T = t[n+1]
-                    d = generate_next_D(d, d.T);
-                    i = 1;
+                    if (d.T > Tsim) {
+                        finish = 1;
+                        i = 1;
+                        break;
+                    }
+                    D_happen(fout);
                 }
             }
             
@@ -142,32 +161,18 @@ int main() {
                         break;
                     }
                     
-                    if (a_event_queue[i].bM == 1) {
-                        DEvent DGEN = {a_event_queue[i].T, Vdlast * (int)(a_event_queue[i].V >= a_event_queue[i].Vth), random_double(0.1, 0.3), -1};
-                        d = DGEN;
-                        fprintf(fout, "%.3lf  A(%.3lf %.3lf %.3lf %-2d)  D(%d %.3lf %-2d)\n", a_event_queue[i].T, a_event_queue[i].V, a_event_queue[i].Vth, a_event_queue[i].dT, a_event_queue[i].bM, d.V, d.dT, d.bM);
-                        Vdlast = d.V;
-                        d_prev = d;
-                        d = generate_next_D(d, a_event_queue[i].T);
-                        a_event_queue[0] = a_event_queue[i];
-                        i = 1;
+                    if (continue_all_A(fout)) {
                         break;
-                    } else {
-                        fprintf(fout, "%.3lf  A(%.3lf %.3lf %.3lf %-2d)\n", a_event_queue[i].T, a_event_queue[i].V, a_event_queue[i].Vth, a_event_queue[i].dT, a_event_queue[i].bM);
                     }
                 }
                 // no bM=1 in A's total progress from t[n] to t[n+1] ---- A simulation bM=-1 at t[n+1]
                 if ((i * dT_A) > d_prev.dT) {
-                    AEvent AGEN = {d.T, d.V, dT_A, Vth, -1};
-                    a = AGEN;
-                    fprintf(fout, "%.3lf  A(%.3lf %.3lf %.3lf %-2d)  D(%d %.3lf %-2d)\n", d.T, a.V, a.Vth, a.dT, a.bM, d.V, d.dT, d.bM);
-                    Vdlast = d.V;
-                    a_prev = a;
-                    a_event_queue[0] = a;
-                    d_prev = d;
-                    a = generate_next_A(a, d.T, Vth);  // d.T = t[n+1]
-                    d = generate_next_D(d, d.T);
-                    i = 1;
+                    if (d.T > Tsim) {
+                        finish = 1;
+                        i = 1;
+                        break;
+                    }
+                    D_happen(fout);
                 }
             }
         }
@@ -176,7 +181,7 @@ int main() {
     // Add total time output before closing
     gettimeofday(&total_end, NULL);
     double total_time = (total_end.tv_sec - total_start.tv_sec) + (total_end.tv_usec - total_start.tv_usec) / 1000000.0;
-    printf("End Timing!\n\n*********TEST RESULT**********\nTotal Time:\t%.4f seconds\nWaiting Time:\t%.4f seconds\nRunning Time:\t%.4f seconds\n", total_time, wait_time/1000.0, total_time - wait_time/1000.0);
+    printf("End Timing!\n\n*********TEST RESULT**********\nTotal Time:  \t%.4f seconds\nWaiting Time:\t%.4f seconds\nRunning Time:\t%.4f  seconds\n", total_time, wait_time/1000.0, total_time - wait_time/1000.0);
     
     fprintf(fout, "%.3lf  FINISH\n", Tsim);
     fclose(fout);
--- PJ2/BASIC_METHOD2_LeapFrogSES/simulator.h
+++ PJ2/BASIC_METHOD2_LeapFrogSES/simulator.h
@@ -3,28 +3,23 @@
 
 #define MAX_EVENTS_A 10000
 
-// A事件定义
 typedef struct {
-    double T;    // 事件发生的时间点
-    double V;    // 值，范围[0,1]
-    double dT;   // 下一个事件的时间间隔
-    double Vth;  // 阈值
-    int bM;      // 1：产生M事件，-1：由M事件产生，0：无关
+    double T;
+    double V;
+    double dT;
+    double Vth;
+    int bM;
 } AEvent;
 
-// D事件定义
 typedef struct {
-    double T;    // 事件发生的时间点
-    int V;       // 值，只能为0或1
-    double dT;   // 下一个事件的时间间隔
-    int bM;      // 1：产生M事件，-1：由M事件产生，0：无关
+    double T;
+    int V;
+    double dT;
+    int bM;
 } DEvent;
 
-// 全局变量
 extern AEvent a_event_queue[MAX_EVENTS_A];
 
-// 函数声明
-double random_double(double min, double max);
 AEvent generate_next_A(AEvent prev, double T, double Vth);
 DEvent generate_next_D(DEvent prev, double T);
 
